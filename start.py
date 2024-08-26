c48cd6228f2517da30cff040c64b8c496501feef
Aniket Raj Singh <mohitrajingh223@gmail.com>
2024-08-26 21:09:32 +0530

    updated start.py

--- start.py
+++ start.py
@@ -1,15 +1,7 @@
-import os
 import subprocess
 
-def activate_virtualenv_and_runserver():
-    # Path to the virtual environment
-    venv_path = os.path.join(os.getcwd(), ".venv/bin/activate")
-
-    try:
-        # Activate the virtual environment and run the Django server
-        subprocess.run(f"source {venv_path} && python manage.py runserver", shell=True, check=True)
-    except subprocess.CalledProcessError as e:
-        print(f"Error occurred: {e}")
+def runserver():
+    subprocess.run("source .venv/bin/activate && python manage.py runserver", shell=True)
 
 if __name__ == "__main__":
-    activate_virtualenv_and_runserver()
+    runserver()
